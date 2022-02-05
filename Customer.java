a8eee6f00c9f021915b9646cf4e00752c61ee561
hilario0306 <58097308+hilario0306@users.noreply.github.com>
2022-02-05 18:16:07 -0300

    Commit 4
    Commit 4

--- Customer.java
+++ Customer.java
@@ -12,11 +12,11 @@ public class Customer {
    public void addRental(Rental arg) {
       _rentals.addElement(arg);
    }
-   
+
    public String getName (){
       return _name;
    }
-  
+
   public String statement() {
      double totalAmount = 0;
      int frequentRenterPoints = 0;
@@ -27,7 +27,7 @@ public class Customer {
         Rental each = (Rental) rentals.nextElement();
 
         //determine amounts for each line
-        double thisAmount = amountFor(each);
+        double thisAmount = each.getCharge();
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
@@ -46,24 +46,5 @@ public class Customer {
              " frequent renter points";
      return result;
    }
-   
-   private double amountFor(Rental aRental) {
-      double thisAmount = 0;
-      switch (aRental.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (aRental.getDaysRented() > 2)
-                 thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += aRental.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (aRental.getDaysRented() > 3)
-                 thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-               break;
-        }
-    return thisAmount;
-   }
+
 }
